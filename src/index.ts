8a2f2c3b27bf7c286a6f56170b1ea9a7c0d8b751
HotariTobu <hotari24tools@gmail.com>
2025-05-05 04:50:04 +0900

    upd: to add custom menu

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,32 @@
 const hello = () => {
   Logger.log('Hello World!')
 }
+
+// @ts-expect-error TS6133
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const onOpen = () => {
+  SpreadsheetApp.getUi()
+    .createMenu('Custom Menu')
+    .addItem('Show dialog', 'showDialog')
+    .addItem('Show sidebar', 'showSidebar')
+    .addToUi()
+}
+
+// @ts-expect-error TS6133
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const showDialog = () => {
+  const html = HtmlService.createHtmlOutputFromFile('dialog')
+    .setWidth(400)
+    .setHeight(300)
+  SpreadsheetApp.getUi().showModalDialog(html, 'My custom dialog')
+}
+
+// @ts-expect-error TS6133
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const showSidebar = () => {
+  const html =
+    HtmlService.createHtmlOutputFromFile('sidebar').setTitle(
+      'My custom sidebar'
+    )
+  SpreadsheetApp.getUi().showSidebar(html)
+}
