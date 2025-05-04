117c84df59a9b7e3758f3f3e60184d33af551ffb
HotariTobu <hotari24tools@gmail.com>
2025-05-05 02:39:44 +0900

    upd: to input html files in src

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { globSync } from 'node:fs'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname, 'src'),
+  publicDir: path.resolve(__dirname, 'public'),
+
+  build: {
+    rollupOptions: {
+      input: globSync(path.resolve(__dirname, 'src/*/index.html')),
+    },
+
+    copyPublicDir: false,
+
+    emptyOutDir: false,
+    outDir: path.resolve(__dirname, 'dest'),
+  },
 })
